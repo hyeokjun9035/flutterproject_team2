f952f5df70f7f45f6c1697bb176b8754da9fad91
gatsby6060 <uniline123@naver.com>
2025-12-29 12:09:03 +0900

    251229 12시08분 전경환커밋푸쉬

---
+++ lib/main_bak1.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'firebase_options.dart';
+import 'home/home_page.dart';
+import 'join/login.dart'; // ✅ LoginPage 파일 경로에 맞게 수정!
+import 'package:flutter_project/community/Community.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // .env 없을 수도 있으면 try/catch로 안전하게
+  try {
+    await dotenv.load(fileName: '.env');
+  } catch (_) {}
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // ✅ Functions 에뮬레이터로 연결 (개발할 때만)
+  FirebaseFunctions.instanceFor(region: 'asia-northeast3')
+      .useFunctionsEmulator(Platform.isAndroid ? '10.0.2.2' : 'localhost', 5001);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Dashboard',
+      debugShowCheckedModeBanner: false,
+
+      // ✅ 라우트는 유지하되, 시작은 로그인으로
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/community': (context) => const CommunityPage(),
+        // '/mypage': (context) => const MyPage(),
+        // '/notice': (context) => const NoticePage(),
+      },
+
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF1976D2)),
+      ),
+    );
+  }
+}
