4a49e56e02fe40866011c83afff351e1e6542210
gatsby6060 <uniline123@naver.com>
2026-01-05 17:00:49 +0900

    전경환 260105 17시00분 커밋

--- lib/admin/admin_home_page.dart
+++ lib/admin/admin_home_page.dart
@@ -463,3 +463,4 @@ class _SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) { return TextField(onChanged: onChanged, decoration: InputDecoration(hintText: hintText, prefixIcon: const Icon(Icons.search), filled: true, fillColor: Colors.grey.shade100, border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none))); }
 }
+
