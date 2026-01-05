b713e711331d170cdc6a12a452d92cc1bb1dc974
Html <sinsk6705@naver.com>
2026-01-05 18:08:00 +0900

    2026-01-05 최종

--- lib/join/sign_step1.dart
+++ lib/join/sign_step1.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'join2.dart';
+import 'sign_step2.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async{
--- lib/join/sign_step4.dart
+++ lib/join/sign_step4.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'joinFinal.dart';
+import 'sign_complete.dart';
 
 
 void main() async{
