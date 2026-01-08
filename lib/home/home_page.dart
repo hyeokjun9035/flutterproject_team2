efbd4a913dc2a850560a46b86379251c3e358f63
kwonhyeokjun <gurwns9035@naver.com>
2026-01-08 16:55:58 +0900

    260108 16:55

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:math';
 import 'dart:async';
+import 'package:cloud_functions/cloud_functions.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
